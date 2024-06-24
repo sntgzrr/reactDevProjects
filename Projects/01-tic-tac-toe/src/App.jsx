4f727910da21d67526b46691a885d22bffa5bda3
sntgzrr <santilozanoc077@gmail.com>
2024-06-24 14:37:34 -0500

    Tic tac toe project added

---
+++ Projects/01-tic-tac-toe/src/App.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { Square } from './components/Square.jsx'
+import { TURNS, } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
+
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState( () =>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    resetGameStorage()
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    saveGameToStorage({board: newBoard, turn: newTurn})
+
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset</button>
+      <section className='game'>
+        {
+          board.map((value, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {value}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <section>
+        <WinnerModal
+          winner={winner}
+          resetGame={resetGame}
+        >
+
+        </WinnerModal>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ Projects/01-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,17 @@
+import { WINNER_COMBOS } from '../constants.js'
+
+export const checkWinnerFrom = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo
+        if (boardToCheck[a] &&
+            boardToCheck[a] === boardToCheck[b] &&
+            boardToCheck[a] === boardToCheck[c]) {
+            return boardToCheck[a]
+        }
+    }
+    return null
+}
+
+export const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square != null)
+}
