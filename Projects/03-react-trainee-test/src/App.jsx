3dccd8f545f25f6e11c87c3b5bb947c638680667
sntgzrr <santilozanoc077@gmail.com>
2024-06-25 17:43:15 -0500

    Trainee test added

---
+++ Projects/03-react-trainee-test/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+export function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ', 3).join()
+    console.log(firstWord)
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(data => {
+        const { url } = data
+        setImageUrl(url)
+      })
+  }, [fact])
+
+  return (
+    <main className='body-element'>
+      <h1>App de AA</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image Extracted ${fact}`} />}
+    </main>
+  )
+}
